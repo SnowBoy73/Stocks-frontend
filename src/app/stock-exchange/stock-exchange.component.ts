840a9c35e90b379e475e45699546fdbfd06c6296
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-03-20 14:57:07 +0100

    subcribe in SEcomponent

--- src/app/stock-exchange/stock-exchange.component.ts
+++ src/app/stock-exchange/stock-exchange.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {StockExchangeService} from './shared/stock-exchange.service';
+import {subscriptionLogsToBeFn} from 'rxjs/internal/testing/TestScheduler';
+import {Subject, Subscription} from 'rxjs';
+import {take, takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'app-stocks',
     templateUrl: './stock-exchange.component.html',
     styleUrls: ['./stock-exchange.component.scss']
 })
-export class StockExchangeComponent implements OnInit {
+export class StockExchangeComponent implements OnInit, OnDestroy {
     stock = new FormControl('');
     stocks: string[] = [];
+    unsubscriber$ = new Subject();
     constructor(private stockExchangeService: StockExchangeService) { }
 
     ngOnInit(): void {
         this.stockExchangeService.listenForStocks()
+            .pipe(
+                takeUntil(this.unsubscriber$)
+            )
             .subscribe(stock => {
+                console.log('helloooo');
                 this.stocks.push(stock);
             });
+    }
+
+    ngOnDestroy(): void {
+        console.log('Destroyed');
+        this.unsubscriber$.next();
+        this.unsubscriber$.complete();
     }
 
     increaseValue(): void {
